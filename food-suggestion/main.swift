43bfe83c43078eeb4b0beca3f951e9fa9e9b699d
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-22 00:03:24 -0500

    Initial Commit

---
+++ food-suggestion/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  food-suggestion
+//
+//  Created by Jaehyuk Rhee on 12/22/18.
+//  Copyright © 2018 Jaehyuk Rhee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
